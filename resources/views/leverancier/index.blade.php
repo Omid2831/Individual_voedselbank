748f448efcb2b54a144c35d50b8cda4fad51ff37
Andrei Gogan <andrei.gogan1@gmail.com>
2026-04-10 14:49:51 +0200

    feat: add warning icon and auto-dismiss to leverancier filter warning

--- resources/views/leverancier/index.blade.php
+++ resources/views/leverancier/index.blade.php
@@ -26,7 +26,10 @@
                     </div>
 
                     <!-- Warning Message -->
-                    <div id="warningMessage" class="hidden mb-4 bg-yellow-100 border-l-4 border-yellow-500 text-yellow-700 p-4 rounded">
+                    <div id="warningMessage" class="hidden mb-4 bg-yellow-100 border-l-4 border-yellow-500 text-yellow-700 p-4 rounded flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-3 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+                        </svg>
                         <p class="font-medium">Er zijn geen leveranciers bekend van het geselecteerde leverancierstype</p>
                     </div>
 
@@ -77,8 +80,7 @@
                                     <td class="px-6 py-4 border-b text-center">
                                         <a href="{{ route('leverancier.products', $leverancier->Id) }}" class="text-blue-600 hover:text-blue-800 focus:outline-none">
                                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                                             </svg>
                                         </a>
                                     </td>
@@ -193,6 +195,11 @@
             // Show warning if selected type has no leveranciers with valid email
             if (selectedType !== '' && selectedType !== 'Selecteer LeverancierType' && hasValidEmailCount === 0) {
                 warningMessage.classList.remove('hidden');
+                
+                // Auto-hide warning after 5 seconds
+                setTimeout(() => {
+                    warningMessage.classList.add('hidden');
+                }, 5000);
             } else {
                 warningMessage.classList.add('hidden');
             }
