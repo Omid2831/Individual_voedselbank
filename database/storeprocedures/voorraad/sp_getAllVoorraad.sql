7f25b385f544b51f46d2183d16124c71611cf8cf
Omid2831 <mehrabiomid8282@gmail.com>
2026-04-10 14:21:59 +0200

    feat: update sp_getAllVoorraad procedure to include additional product fields and formatted dates

--- database/storeprocedures/voorraad/sp_getAllVoorraad.sql
+++ database/storeprocedures/voorraad/sp_getAllVoorraad.sql
@@ -5,11 +5,18 @@ DELIMITER $$
 CREATE PROCEDURE sp_getAllVoorraad()
 BEGIN
     SELECT 
+        p.Id AS ProductId,
         p.Naam AS ProductNaam,
+        p.Barcode,
+        p.Houdbaarheidsdatum,
+        p.Ontvangstdatum,
         c.Naam AS Categorie,
         m.VerpakkingsEenheid AS Eenheid,
         m.Aantal,
-        DATE_FORMAT(p.Houdbaarheidsdatum, '%d-%m-%Y') AS Houdbaarheidsdatum,
+        m.Uitleveringsdatum,
+        DATE_FORMAT(p.Houdbaarheidsdatum, '%d-%m-%Y') AS HoudbaarheidsdatumFormatted,
+        DATE_FORMAT(p.Ontvangstdatum, '%d-%m-%Y') AS OntvangstdatumFormatted,
+        DATE_FORMAT(m.Uitleveringsdatum, '%d-%m-%Y') AS UitleveringsdatumFormatted,
         ppm.Locatie AS Magazijn
     FROM product AS p
     LEFT JOIN Categorie AS c ON p.CategorieId = c.Id
