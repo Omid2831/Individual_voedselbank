c70b15cc7cc58ef68d1bbe9ef612349a106e56fe
Omid2831 <mehrabiomid8282@gmail.com>
2026-04-10 13:26:29 +0200

    feat: update sp_getAllVoorraad procedure to include packaging unit and correct quantity retrieval

--- database/storeprocedures/voorraad/sp_getAllVoorraad.sql
+++ database/storeprocedures/voorraad/sp_getAllVoorraad.sql
@@ -7,15 +7,15 @@ BEGIN
     SELECT 
         p.Naam AS ProductNaam,
         c.Naam AS Categorie,
-        COUNT(ppl.Id) AS Aantal,
+        m.VerpakkingsEenheid AS Eenheid,
+        m.Aantal,
         DATE_FORMAT(p.Houdbaarheidsdatum, '%d-%m-%Y') AS Houdbaarheidsdatum,
-        l.Naam AS Magazijn
+        ppm.Locatie AS Magazijn
     FROM product AS p
     LEFT JOIN Categorie AS c ON p.CategorieId = c.Id
-    LEFT JOIN ProductPerLeverancier AS ppl ON p.Id = ppl.ProductId
-    LEFT JOIN Leverancier AS l ON ppl.LeverancierId = l.Id
-    GROUP BY p.Id, p.Naam, c.Naam, p.Houdbaarheidsdatum, l.Naam
-    ORDER BY p.Houdbaarheidsdatum DESC, Aantal DESC;
+    LEFT JOIN ProductPerMagazijn AS ppm ON p.Id = ppm.ProductId
+    LEFT JOIN Magazijn AS m ON ppm.MagazijnId = m.Id
+    ORDER BY p.Houdbaarheidsdatum DESC;
 END $$
 
 DELIMITER ;
