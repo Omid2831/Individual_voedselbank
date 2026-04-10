53d8a44e6291518166d22186820077357581fbe4
Andrei Gogan <andrei.gogan1@gmail.com>
2026-04-10 14:50:23 +0200

    refactor: improve products method with better error logging and direct DB query

--- app/Http/Controllers/LeverancierController.php
+++ app/Http/Controllers/LeverancierController.php
@@ -29,26 +29,34 @@ class LeverancierController extends Controller
     public function products($id)
     {
         try {
-            // Get leverancier details
-            $leveranciers = DB::select('CALL sp_getAllLeverancier()');
-            $leverancier = collect($leveranciers)->firstWhere('Id', $id);
+            // Get leverancier details directly from database
+            $leverancier = DB::table('Leverancier')
+                ->where('Id', $id)
+                ->where('IsActive', 1)
+                ->first();
             
             if (!$leverancier) {
+                Log::warning('Leverancier not found with ID: ' . $id);
                 return redirect()
                     ->route('leverancier.index')
                     ->with('error', 'Leverancier niet gevonden');
             }
             
-            // Get products for this leverancier
+            Log::info('Found leverancier: ' . $leverancier->Naam);
+            
+            // Get products for this leverancier using stored procedure
             $products = DB::select('CALL sp_getProductsByLeverancier(?)', [$id]);
+            
+            Log::info('Found ' . count($products) . ' products');
             
             return view('leverancier.products', compact('leverancier', 'products'));
         } catch (Exception $e) {
             Log::error('Error fetching products for leverancier ' . $id . ': ' . $e->getMessage());
+            Log::error('Stack trace: ' . $e->getTraceAsString());
             
             return redirect()
                 ->route('leverancier.index')
-                ->with('error', 'Er is een fout opgetreden bij het ophalen van de producten.');
+                ->with('error', 'Er is een fout opgetreden bij het ophalen van de producten: ' . $e->getMessage());
         }
     }
     
