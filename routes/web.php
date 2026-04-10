2aebee57c2dbcd3aca82680200d7623aaa3ab507
DannyEdelbroek <334855@student.mboutrecht.nl>
2026-04-10 15:34:48 +0200

    feat: add routes for voedselpakketten management by vrijwilligers

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\VoedselpakketController;
 use App\Http\Controllers\ManagerController;
 
 Route::get('/', function () {
@@ -25,6 +26,12 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+// voedselpakketten routes
+Route::get('/vrijwilliger/voedselpakketten', [VoedselpakketController::class, 'index'])->middleware(['auth', 'verified', 'role:manager,vrijwilliger'])->name('voedselpakket.index');
+Route::get('/vrijwilliger/show/{id}', [VoedselpakketController::class, 'show'])->middleware(['auth', 'verified', 'role:manager,vrijwilliger'])->name('voedselpakket.show');
+Route::get('/vrijwilliger/edit/{id}', [VoedselpakketController::class, 'edit'])->middleware(['auth', 'verified', 'role:manager,vrijwilliger'])->name('voedselpakket.edit');
+Route::put('/vrijwilliger/update/{id}', [VoedselpakketController::class, 'update'])->middleware(['auth', 'verified', 'role:manager,vrijwilliger'])->name('voedselpakket.update');
 
 
 require __DIR__.'/auth.php';
