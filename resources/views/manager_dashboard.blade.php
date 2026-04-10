474566b245fc6d010f32fdb548107b9f1ed18aa9
Andrei Gogan <andrei.gogan1@gmail.com>
2026-04-10 13:34:28 +0200

    feat: add Overzicht Leveranciers link to manager dashboard

--- resources/views/manager_dashboard.blade.php
+++ resources/views/manager_dashboard.blade.php
@@ -9,12 +9,10 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }} <br>
-                    {{ "This is the manager page" }}
-                    @dump([
-                        'username' => auth()->user()->name,
-                        'role' => auth()->user()->role,
-                    ])
+                    <h3 class="text-2xl font-bold mb-4">Homepage voedselbank maaskantje</h3>
+                    <a href="{{ route('leverancier.index') }}" class="text-blue-600 hover:text-blue-800 underline text-lg">
+                        Overzicht Leveranciers
+                    </a>
                 </div>
             </div>
         </div>
