d915d18429a1715ea45914ba05a540b8728b8541
NeroTk345 <Neroterkuile@gmail.com>
2026-04-10 14:05:01 +0200

    create(sp): made stored procedures for edit allergies

--- app/Models/Allergie.php
+++ app/Models/Allergie.php
@@ -14,10 +14,10 @@ class Allergie extends Model
      * @param string|null $allergieNaam
      * @return array
      */
-    public static function getGezinnenMetAllergieen($allergieNaam = null)
-    {
-        return DB::select('CALL Sp_GetAllAllergieen(?)', [$allergieNaam]);
-    }
+public static function getGezinnenMetAllergieen($allergieNaam = null)
+{
+    return DB::select('CALL Sp_GetAllAllergieen(?)', [$allergieNaam]);
+}
 
     /**
      * Haal alle actieve allergieën op voor de dropdown.
@@ -27,5 +27,40 @@ class Allergie extends Model
     public static function getAllAllergies()
     {
         return DB::table('Allergie')->where('IsActive', 1)->get();
+    }
+
+    /**
+     * Haal alle personen van een gezin en hun bijbehorende allergieën op.
+     * 
+     * @param int $gezinId
+     * @return array
+     */
+    public static function getGezinAllergieDetails($gezinId)
+    {
+        return DB::select('CALL Sp_GetAllergieenDetailsPerGezin(?)', [$gezinId]);
+    }
+
+    /**
+     * Haal een persoon-allergie record op voor het bewerken.
+     * 
+     * @param int $persoonId
+     * @return object|null
+     */
+    public static function getPersoonAllergie($persoonId)
+    {
+        $result = DB::select('CALL Sp_GetPersoonAllergieForEdit(?)', [$persoonId]);
+        return count($result) > 0 ? $result[0] : null;
+    }
+
+    /**
+     * Wijzig de allergie van een specifiek persoon.
+     * 
+     * @param int $allergiePerPersoonId
+     * @param int $newAllergieId
+     * @return void
+     */
+    public static function updatePersoonAllergie($allergiePerPersoonId, $newAllergieId)
+    {
+        DB::select('CALL Sp_UpdatePersoonAllergie(?, ?)', [$allergiePerPersoonId, $newAllergieId]);
     }
 }
