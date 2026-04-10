2ed02a9c0d7b820dc8c7f30c7a4bd17d0e843efd
Omid2831 <mehrabiomid8282@gmail.com>
2026-04-10 12:33:38 +0200

    feat: add route for voorraad overview in web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ManagerController;
+use App\Http\Controllers\VoorraadController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -24,7 +25,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/voorraad/overzicht', [VoorraadController::class, 'overzicht'])->name('voorraad.overzicht');
 });
 
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
