3e0d27b5bec1bbe45a0722119479fda410d9ae40
NeroTk345 <Neroterkuile@gmail.com>
2026-04-10 15:18:25 +0200

    create(view): modifed the  view pages for allergies so i can see all the family members

--- resources/views/allergeen/allergeen_detail.blade.php
+++ resources/views/allergeen/allergeen_detail.blade.php
@@ -50,7 +50,7 @@
                                 <td class="py-3 px-4 border-r border-gray-100 text-gray-600">{{ $persoon->Gezinsrol }}</td>
                                 <td class="py-3 px-4 border-r border-gray-100 text-gray-600">{{ $persoon->Allergie }}</td>
                                 <td class="py-3 px-4 text-center">
-                                    <a href="{{ route('allergieen_edit', ['id' => $persoon->PersoonId]) }}" class="bg-blue-500 hover:bg-blue-600 text-white rounded px-2 py-1 inline-flex items-center justify-center transition shadow-sm">
+                                    <a href="{{ route('allergieen_edit', ['persoon_id' => $persoon->PersoonId]) }}" class="bg-blue-500 hover:bg-blue-600 text-white rounded px-2 py-1 inline-flex items-center justify-center transition shadow-sm">
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                                         </svg>
