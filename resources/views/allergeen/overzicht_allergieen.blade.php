e4f1c152c98ebd1ac5832c535cd23600407f802d
NeroTk345 <Neroterkuile@gmail.com>
2026-04-10 14:06:43 +0200

    modified(view): added routing to the edit section

--- resources/views/allergeen/overzicht_allergieen.blade.php
+++ resources/views/allergeen/overzicht_allergieen.blade.php
@@ -60,7 +60,7 @@
                                 <td class="py-3 px-4 border-r border-gray-100 text-center text-gray-600">{{ $gezin->AantalBabys }}</td>
                                 <td class="py-3 px-4 border-r border-gray-100 text-gray-600">{{ $gezin->VertegenwoordigerNaam }}</td>
                                 <td class="py-3 px-4 text-center">
-                                    <a href="#" class="text-blue-500 border border-blue-200 bg-blue-50 hover:bg-blue-100 rounded px-2 py-1 inline-flex items-center justify-center transition">
+                                    <a href="{{ route('allergieen_detail', ['gezin_id' => $gezin->GezinId]) }}" class="text-blue-500 border border-blue-200 bg-blue-50 hover:bg-blue-100 rounded px-2 py-1 inline-flex items-center justify-center transition">
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                                         </svg>
@@ -76,6 +76,9 @@
                             @endforelse
                         </tbody>
                     </table>
+                    <div class="mt-4">
+    {{ $gezinnen->links() }}
+</div>
                 </div>
 
                 <!-- Home knop rechtsonder uit wireframe -->
