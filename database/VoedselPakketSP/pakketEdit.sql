8462e70d49e16e6bca5d79346dac2a588afe395f
DannyEdelbroek <334855@student.mboutrecht.nl>
2026-04-13 23:45:07 +0200

    fix: enhance spGetPakketVoorEdit procedure to include Gezinsnaam in the selection

--- database/VoedselPakketSP/pakketEdit.sql
+++ database/VoedselPakketSP/pakketEdit.sql
@@ -7,9 +7,13 @@ CREATE PROCEDURE spGetPakketVoorEdit(
     IN p_PakketId INT
 )
 BEGIN
+    -- We halen direct de data op. De logica voor de blokkade 
+    -- en de meldingen regelen we in de Controller en de Blade.
     SELECT 
         VP.Id AS PakketId,
-        VP.Status
+        VP.GezinId,
+        VP.Status,
+        G.Naam AS Gezinsnaam -- Optioneel: de naam van het gezin erbij
     FROM Voedselpakket VP
     INNER JOIN Gezin G ON VP.GezinId = G.Id
     WHERE VP.Id = p_PakketId;
